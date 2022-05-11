452c4d6631b419064bbb7e1346f03fde98c361e0
kavinrajsi <sikavinraj@gmail.com>
2022-05-11 13:09:17 +0530

    updated menu action

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,129 +1,129 @@
-console.log('\'Allo \'Allo!');
+console.log("'Allo 'Allo!");
 
 // service worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('./sw.js')
-//     .then(serviceWorker => {
-//       console.log('Service Worker registered: ', serviceWorker);
-//     })
-//     .catch(error => {
-//       console.error('Error registering the Service Worker: ', error);
-//     });
-// }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then((serviceWorker) => {
+      console.log("Service Worker registered: ", serviceWorker);
+    })
+    .catch((error) => {
+      console.error("Error registering the Service Worker: ", error);
+    });
+}
 
 // image lazy load
-if ('loading' in HTMLImageElement.prototype) {
+if ("loading" in HTMLImageElement.prototype) {
   const images = document.querySelectorAll('img[loading="lazy"]');
   images.forEach((img) => {
     img.src = img.dataset.src;
   });
 } else {
   // Dynamically import the LazySizes library
-  const script = document.createElement('script');
+  const script = document.createElement("script");
   script.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js";
   document.body.appendChild(script);
 }
 
-let text = document.querySelector('.circle-text strong');
-// console.log(text);
-text.innerHTML = text.innerText.split('').map(
-  (char, i) =>
-    `<span style="transform: rotate(${i * 12}deg)">${char}</span>`
-).join('');
+let text = document.querySelector(".circle-text strong");
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) => `<span style="transform: rotate(${i * 12}deg)">${char}</span>`
+  )
+  .join("");
 
-let text2 = document.querySelector('.circle-text2 strong');
-console.log(text2);
+let text2 = document.querySelector(".circle-text2 strong");
 text2.innerHTML = text2.innerText
-  .split('')
+  .split("")
   .map(
     (char, i) => `<span style="transform: rotate(${i * 12}deg)">${char}</span>`
   )
-  .join('');
+  .join("");
 
-let text3 = document.querySelector('.circle-text3 strong');
-// console.log(text);
+let text3 = document.querySelector(".circle-text3 strong");
 text3.innerHTML = text3.innerText
-  .split('')
+  .split("")
   .map(
     (char, i) => `<span style="transform: rotate(${i * 12}deg)">${char}</span>`
   )
-  .join('');
+  .join("");
 
-  	// Show menu in mobile
-	$('.site-menu-icon').click(function () {
-    var self = $(this);
-    var body = $('body');
-    var siteMenu = $('.header-site-nav');
+// Show menu in mobile
+$(".site-menu-icon").click(function () {
+  var self = $(this);
+  var body = $("body");
+  var siteMenu = $(".header-site-nav");
+  var siteMenuIcon = $(".site-menu-icon");
 
-    if (!self.hasClass('open')) {
-      self.addClass('open');
-      siteMenu.addClass('open show');
-      body.addClass('no-scroll');
-    } else {
-      console.log('else');
-      self.removeClass('open');
-      siteMenu.removeClass('open');
-      siteHeader.removeClass('open');
-      body.removeClass('no-scroll');
-      setTimeout(function () {
-        siteMenu.removeClass('show');
-      }, 200);
-    }
-  });
+  if (!self.hasClass("open")) {
+    self.addClass("open");
+    siteMenu.addClass("open show");
+    body.addClass("no-scroll");
+  } else {
+    console.log("else");
+    self.removeClass("open");
+    siteMenu.removeClass("open show");
+    siteMenuIcon.removeClass("open show");
+    body.removeClass("no-scroll");
+  }
+});
 
-	// // submenu open
-	// $(".menu-item-has-children > a").click(function (e) {
-	// 	e.preventDefault();
-	// 	$(".menu-item-has-children").toggleClass("active");
-	// 	$(".menu-item-has-children .sub-menu").toggleClass("open");
-	// });
+$(".header-site-nav-list a").click(function () {
+  var self = $(this);
+  var body = $("body");
+  var siteMenu = $(".header-site-nav");
+  var siteMenuIcon = $(".site-menu-icon");
 
-	// $(document).click(function (e) {
-	// 	$(".menu-item-has-children").each(function () {
-	// 		var container = $(".menu-item-has-children");
-	// 		if (!container.is(e.target) && container.has(e.target).length === 0) {
-	// 			$(this).removeClass("active");
-	// 			$(".menu-item-has-children .sub-menu").removeClass("open");
-	// 		}
-	// 	});
-	// });
-
+  if (!body.hasClass("no-scroll")) {
+  } else {
+    console.log('admo');
+    self.removeClass("open");
+    siteMenu.removeClass("open show");
+    siteMenuIcon.removeClass("open show");
+    body.removeClass("no-scroll");
+  }
+});
 
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(':focus')) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
       }
     }
   });
